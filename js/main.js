419a616e8e4a27e8dc220c82acf7de37dd2060a1
MozzH <72160112+MozzH@users.noreply.github.com>
2021-12-09 15:42:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+/* Hamburger Menu on Click function */
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+
